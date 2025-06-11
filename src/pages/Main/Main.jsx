c7c7328377840411774b38f7df1bbb66ffff1a3e
enzovalencuela <esilvavalencuela@gmail.com>
2025-06-11 16:49:56 -0400

    Ajustando frase

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -14,7 +14,7 @@ const Main = () => {
             </h2>
             <img
               id="frase-cabecalho"
-              src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&size=17&pause=1000&color=686868&center=true&width=480&lines=Seja+bem+vindo+ao+meu+portifólio!;Sou+Desenvolvedor+Front+End"
+              src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&size=22&pause=1000&color=686868&center=true&width=480&lines=Seja+bem+vindo+ao+meu+portifólio!;Sou+Desenvolvedor+Front+End"
               alt="Typing SVG"
             />
           </div>
