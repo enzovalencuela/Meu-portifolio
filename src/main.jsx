26e1daca8e0faa344b28a80c14e9572267f5833f
enzovalencuela <esilvavalencuela@gmail.com>
2025-08-24 08:59:45 -0400

    Ajustando cards de projeto no mobile

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,6 @@ register();
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
